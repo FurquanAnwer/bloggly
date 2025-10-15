125cad8c16957a893bd2a20f39d9e6841a89e793
Furquan Anwer <furquan.anwer@gmail.com>
2025-10-15 22:23:54 +0530

    removed connection string

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://blogapp:sshcetC84W0dnGRm@cluster0.h8bdkam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING_)
     console.log("DB Connected")
 }
 
