2a6007677ae49bff04e42372f43d16a73164b6d6
Furquan Anwer <furquan.anwer@gmail.com>
2025-10-15 17:11:25 +0530

    added header,main body,footer and blog page

--- components/BlogItem.tsx
+++ components/BlogItem.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { assets, blog_data } from "@/assets/assets";
+import Link from "next/link";
 
-const BlogItem = ({title,description,category,image}) => {
+const BlogItem = ({title,description,category,image,id}) => {
   return (
     <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
-        <Image src={image} alt='' width={400} height={400} className="border-b border-black"/>
+        <Link href={`/blogs/${id}`}>
+            <Image src={image} alt='' width={400} height={400} className="border-b border-black"/>
+        </Link>
         <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">
             {category}
         </p>
@@ -15,10 +18,12 @@ const BlogItem = ({title,description,category,image}) => {
             <p className="mb-3 text-sm tracking-tight text-gray-700">
                 {description}
             </p>
-            <div className="inline-flex items-center py-2 font-semibold text-center">
-                Read more
-                <Image src={assets.arrow} alt = '' width={12} className="ml-2"/>
-            </div>
+            <Link href={`/blogs/${id}`}>
+                <div className="inline-flex items-center py-2 font-semibold text-center">
+                    Read more
+                    <Image src={assets.arrow} alt = '' width={12} className="ml-2"/>
+                </div>
+            </Link>
         </div>
     </div> 
   );
