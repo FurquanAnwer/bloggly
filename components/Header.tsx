b58d347bbf93643ce044e7f74a9f3a0c6b0864e4
Furquan Anwer <furquan.anwer@gmail.com>
2025-10-24 12:39:24 +0530

    create email subscription feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
 import {assets} from '@/assets/assets'
 import Link from "next/link";
+import { toast } from "react-toastify";
 
 const Header = () => {
+    const [email,setEmail] = useState("")
+    const onSubmitHandler = async (e) =>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append("email",email)
+        const response = await axios.post('/api/email',formData)
+        if(response.data.success){
+            toast.success(response.data.msg)
+            setEmail("")
+        }
+        else{
+            toast.error("Error")
+        }
+    }
   return (
     <div className="py-5 px-5 md:px-12 lg:px-28">
         <div className="flex justify-between items-center">
@@ -21,14 +36,12 @@ const Header = () => {
             <p className="mt-10 max-w-[740] m-auto text-xs sm:text-base">
                 Lorem Ipsum is simply dummy text of this blog post.
             </p>
-            <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]">
-                <input type="email" placeholder="Enter your email" className="pl-4 outline-none"/>
+            <form onSubmit={onSubmitHandler} className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]">
+                <input onChange={(e)=>setEmail(e.target.value)}value={email} type="email" placeholder="Enter your email" className="pl-4 outline-none"/>
                 <button type="submit" className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">
                     Subscribe
                 </button>
             </form>
-
-
         </div>
     </div>
   );
